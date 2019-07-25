7ddfc834a239591eb660510e643c142436993eb2
adeadedoja <damdey@yahoo.com>
2019-07-25 20:45:09 +0200

    Blog Listing now working

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,7 +1,28 @@
 <template>
-    
+    <Layout>
+        <br>
+        <g-link to="/blog" class="link">  &larr; Go Back</g-link>
+        <div class="post-title">
+            <h1>{{$page.post.title}}</h1>
+            <p class="post-date"> {{ $page.post.date}} | {{$page.post.timeToRead}} min read
+            </p>
+        </div>
+        <div class="post-content">
+            <p v-html="$page.post.content" />
+        </div>
+    </Layout>
 </template>
-
+<page-query>
+    query Post ($path: String!) {
+        post: post (path: $path) {
+            id
+            title
+            content
+            date (format: "D MMMM YYYY")
+            timeToRead
+        }
+    }
+</page-query>
 <script>
     export default {
         name: "Post"
