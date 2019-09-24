0c1d22b05ee44dabdbabd85e0dcb6aabe4b828bf
adeadedoja <damdey@yahoo.com>
2019-09-24 20:33:53 +0200

    Edited readne

--- README.md
+++ README.md
@@ -1,14 +1,38 @@
-# Default starter for Gridsome
+# Gridsome Agency Starter using Bootstrap 4
 
-This is the project you get when you run `gridsome create new-project`.
+> A minimalistic starter theme for an agency using [Gridsome]() with Bootstrap 4. Uses a plugin to query instagram posts, and pages uses markdown.
 
+## Preview
+
+![preview](https://pbs.twimg.com/media/EFP-n2pWkAEa3si?format=jpg&name=4096x4096)
+
+### Demo
+
+[https://test.dev](https://test.dev)
+
+### Features
+
+This starter project includes some of the latest powerful technologies.
+
+*   **Gridsome ** 
+*   **Bootstrap ** 
+*   **Sass **
+*   **Markdown ** 
+
+## Install
 ### 1. Install Gridsome CLI tool if you don't have
 
 `npm install --global @gridsome/cli`
 
 ### 2. Create a Gridsome project
 
-1. `gridsome create my-gridsome-site` to install default starter
-2. `cd my-gridsome-site` to open the folder
+1. `gridsome create my-agency https://github.com/adeadedoja/gridsome-agency-starter-bootstrap` to install default starter
+2. `cd my-agency` to open the folder
 3. `gridsome develop` to start a local dev server at `http://localhost:8080`
 4. Happy coding 🎉🙌
+
+<!-- Markdown snippet -->
+### 3. Deploy Gridsome project
+Use this deploy button to get your own copy of the repository.
+
+[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/adeadedoja/gridsome-agency-starter-bootstrap)
---
+++ src/components/Footer.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="py-5 row">
+        <div class="col-md-2 col-6">
+            <p><b>QUICKLINKS</b></p>
+            <ul class="list-unstyled font-big-1-2 font-weight-light">
+                <li><g-link to="/">HOME</g-link></li>
+                <li><g-link to="/about">ABOUT</g-link></li>
+                <li><g-link to="/portfolio">PORTFOLIO</g-link></li>
+                <li><g-link to="/blog">BLOG</g-link></li>
+                <li><g-link to="/contact">CONTACT</g-link></li>
+            </ul>
+        </div>
+        <div class="col-md-2 col-6">
+            <p><b>SOCIAL</b></p>
+            <ul class="list-unstyled font-big-1-2 font-weight-light">
+                <li><g-link to="/">Twitter</g-link></li>
+                <li><g-link to="/">Instagram</g-link></li>
+                <li><g-link to="/">Facebook</g-link></li>
+                <li><g-link to="/">Medium</g-link></li>
+            </ul>
+        </div>
+        <div class="col-md-6 offset-md-2 col-12 mt-3 mt-md-0">
+            <div class="form-group mb-5">
+                <label class="font-weight-bold">NEWSLETTER</label>
+                <div class="input-group mb-3 border-bottom">
+                    <input type="text" class="form-control border-0" placeholder="Recipient's username" aria-label="Enter your email address" aria-describedby="button-addon2">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary border-0" type="button" id="button-addon2">SUBSCRIBE</button>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="font-weight-bold">GENERAL ENQUIRIES</label>
+                <h4 class="font-weight-light"><a href="mailto:hello@badman.com">hello@badman.com</a></h4>
+            </div>
+        </div>
+        <div class="container pt-5 text-muted small">
+            <p class="mb-0">&copy; {{ currentYear }} Badman Co. Gridsome Agency Starter by <a href="https://twitter.com/TrussDamola">@TrussDamola</a></p>
+            <p>Powered by <a href="https://gridsome.org/">Gridsome</a> and <a href="https://getbootstrap.com/">Bootstrap 4</a></p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Footer",
+        data() {
+            return {
+                currentYear: new Date().getFullYear()
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
